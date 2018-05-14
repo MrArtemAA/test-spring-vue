9ad01e409821bd9dbb421385301e990a791855f9
Artem Areshko <Artem.Areshko@mvideo.ru>
2018-05-14 19:00:42 +0300

    Initial commit

---
+++ backend/src/main/java/ru/artemaa/test/testspringvue/TestSpringVueApplication.java
@@ -0,0 +1,12 @@
+package ru.artemaa.test.testspringvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringVueApplication.class, args);
+	}
+}
